d0b6f0568854f54d5dffe71b0d5379ed09ddcf2d
JunkyardJackass <junkyardjackass@gmail.com>
2024-08-11 23:53:48 -0600

    Update Encanter.py
    # This SHOULD, and I cant stress how little I KNOW it will work, Spam E on pressing F3.
    # I PERSONALLY configured my mouse to have the F3 key programmed to it.
    # None of this portion is tested and I stole it from ChatGPT but don't have time to test.

--- Encanter.py
+++ Encanter.py
@@ -4,6 +4,7 @@ import pyautogui
 import time
 import threading
 import keyboard
+
 """
 In file ReadMe.(why? because im not explaining.)
     SET UP: SKYRIM ENCHANTING
@@ -32,7 +33,9 @@ Process should auto burn any consumables for the process that are in your invent
 
 Press U to shut the secquence down, or if thats not working because im a lunatic, close the program.
 """
+
 stop_event = threading.Event()
+f3_event = threading.Event()
 
 def K(key):
     if key in ('e', 'w', 'a', 's', 'd', 'r', 'y'):
@@ -44,11 +47,10 @@ def K(key):
             return
 
 def perform_sequence():
-
-""" # WEAPON enchants, requires additional E key command, uncomment to activate.
+    """ # WEAPON enchants, requires additional E key command, uncomment to activate.
     sequence = ['d', 'e', 'a', 's', 'd', 'e', 'e', 'a', 's', 'd', 'e', 'r', 'y',
                 'e', 'a', 'w', 'd', 'e', 'e', 'a', 'w', 'd', 'e', 'r', 'y']
-"""
+    """
     # Armour enchants, requires only one E key command to pass, uncomment to activate.
     sequence = ['d', 'e', 'a', 's', 'd', 'e', 'a', 's', 'd', 'e', 'r', 'y',
                 'e', 'a', 'w', 'd', 'e', 'a', 'w', 'd', 'e', 'r', 'y']
@@ -58,7 +60,18 @@ def perform_sequence():
             if stop_event.is_set():
                 break
             K(key)
-            
+
+# This SHOULD, and I cant stress how little I KNOW it will work, Spam E on pressing F3.
+# I PERSONALLY configured my mouse to have the F3 key programmed to it.
+# None of this portion is tested and I stole it from ChatGPT but don't have time to test.
+def simulate_e_key():
+    while not stop_event.is_set():
+        if keyboard.is_pressed('f3'):
+            f3_event.set()
+            while keyboard.is_pressed('f3') and not stop_event.is_set():
+                pyautogui.press('e')
+                time.sleep(0.025)
+            f3_event.clear()
 
 def monitor_stop_key():
     while not stop_event.is_set():
@@ -76,9 +89,13 @@ sequence_thread.start()
 stop_thread = threading.Thread(target=monitor_stop_key)
 stop_thread.start()
 
+e_key_thread = threading.Thread(target=simulate_e_key)
+e_key_thread.start()
+
 monitor_stop_key()
 
 sequence_thread.join()
 stop_thread.join()
+e_key_thread.join()
 
 print("Sequence stopped.")
